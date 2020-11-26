df6686b718877810c574839d0427e7dddd3c27f8
felipe12121 <69096888+felipe12121@users.noreply.github.com>
2020-11-26 20:41:03 -0300

    Add files via upload

---
+++ vrp_notify/fxmanifest.lua
@@ -0,0 +1,26 @@
+fx_version 'adamant'
+game 'gta5'
+
+ui_page "html/index.html"
+
+author "</Storm>"
+
+client_scripts {
+	"client.lua"
+}
+
+server_scripts {
+	"server.lua"
+}
+
+files {
+	"html/index.html",
+	"html/index.js",
+    "html/index.css",
+    "html/images/1.png",
+	"html/images/2.png",
+	"html/images/3.png",
+	"html/images/4.png",
+	"html/images/5.png",
+	"html/z-arista/Arista2.0light.otf",
+}
